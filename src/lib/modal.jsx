e83fdd75ed3b5d3f0f235d1c960021bded96bf11
Levani migriauli <levani.migriauli@lb.ge>
2024-09-13 23:39:52 +0400

    feat: init & add reusable modal

---
+++ src/lib/modal.jsx
@@ -0,0 +1,47 @@
+import React, { forwardRef, useImperativeHandle, useState } from 'react'
+import Modal from 'react-modal'
+
+Modal.setAppElement('#root')
+
+const CustomModal = forwardRef(({ isOpen, children, padding }, ref) => {
+  const [modalIsOpen, setModalIsOpen] = useState(isOpen || false)
+
+  useImperativeHandle(ref, () => ({
+    handleOpenModal () {
+      setModalIsOpen(true)
+    },
+    handleCloseModal () {
+      setModalIsOpen(false)
+    },
+    isOpened () {
+      return modalIsOpen
+    }
+  }))
+
+  return (
+    <div>
+      <Modal
+        ref={ref}
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        contentLabel="Example Modal"
+        style={{
+          overlay: {
+            backgroundColor: 'rgba(2, 21, 38, 0.34)'
+          },
+          content: {
+            top: '50%',
+            left: '50%',
+            right: 'auto',
+            bottom: 'auto',
+            transform: 'translate(-50%, -50%)',
+            padding: `${padding}`
+          }
+        }}
+      >
+        {children}
+      </Modal>
+    </div>
+  )
+})
+export default CustomModal
