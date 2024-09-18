3ff05c5471e8aa63e163ef73f3473b531c65f82e
Levani migriauli <levani.migriauli@lb.ge>
2024-09-19 00:28:03 +0400

    fix: change filter functionality to work for OR logic(unique options)

--- src/redux/selectors/realEstateFilterSelector.js
+++ src/redux/selectors/realEstateFilterSelector.js
@@ -1,31 +1,56 @@
 import { createSelector } from '@reduxjs/toolkit'
 
+const getUniqueProperties = (properties) => {
+  const uniqueProperties = new Map()
+  properties.forEach(property => uniqueProperties.set(property.id, property))
+  return Array.from(uniqueProperties.values())
+}
+
 const selectRealEstateData = state => state.realEstates
 
 export const selectRealEstateFilterData = createSelector(
-  [
-    selectRealEstateData,
-    (state, dropDownDataForFilter) => dropDownDataForFilter],
+  [selectRealEstateData, (state, dropDownDataForFilter) => dropDownDataForFilter],
   (realEstateData, dropDownDataForFilter) => {
     const { regions, priceRange, area } = dropDownDataForFilter
 
+    const isAnyFilterApplied =
+      regions.length > 0 ||
+      priceRange.min !== '' || priceRange.max !== '' ||
+      area.min !== '' || area.max !== ''
+
+    if (!isAnyFilterApplied) {
+      return realEstateData
+    }
+
     const selectedRegionIds = regions.length > 0 ? regions.map(
       region => region.id) : null
+    const filteredByRegions = selectedRegionIds
+      ? realEstateData.filter(
+        property => selectedRegionIds.includes(property.city.region_id))
+      : []
 
-    return realEstateData.filter(property => {
+    const isPriceRangeSet = priceRange.min !== '' || priceRange.max !== ''
+    const filteredByPrice = isPriceRangeSet
+      ? realEstateData.filter(property => {
+        return (priceRange.min === '' || property.price >=
+            Number(priceRange.min)) &&
+          (priceRange.max === '' || property.price <= Number(priceRange.max))
+      })
+      : []
 
-      const regionsMatch = selectedRegionIds ? selectedRegionIds.includes(
-        property.city.region_id) : true
+    const isAreaSet = area.min !== '' || area.max !== ''
+    const filteredByArea = isAreaSet
+      ? realEstateData.filter(property => {
+        return (area.min === '' || property.area >= Number(area.min)) &&
+          (area.max === '' || property.area <= Number(area.max))
+      })
+      : []
 
-      const priceMatch = (priceRange.min === '' || property.price >=
-          Number(priceRange.min)) &&
-        (priceRange.max === '' || property.price <= Number(priceRange.max))
+    const combinedResults = [
+      ...filteredByRegions,
+      ...filteredByPrice,
+      ...filteredByArea]
 
-      const areaMatch = (area.min === '' || property.area >=
-          Number(area.min)) &&
-        (area.max === '' || property.price <= Number(area.max))
-
-      return priceMatch && regionsMatch && areaMatch
-    })
+    return getUniqueProperties(combinedResults)
   }
 )
