43d2d1782b7c0ebab60b63f61f42e1bce953318d
Levani migriauli <levani.migriauli@lb.ge>
2024-09-21 02:57:01 +0400

    WIP(addListingsPage): replace old react select with custom one and use for regions

--- src/pages/addListingPage/addListingPage.jsx
+++ src/pages/addListingPage/addListingPage.jsx
@@ -1,10 +1,13 @@
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useEffect, useRef, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { useForm } from 'react-hook-form'
 import classes from './addListingPage.module.scss'
 import { getCities } from '../../api/geographicalInfo.js'
 import { setCities } from '../../redux/slices/citiesSlice.js'
 import Input from '../../ui/lib/input.jsx'
+import AddAgentModal from '../homePage/addAgentModal.jsx'
+import CustomDropdown from '../../ui/lib/select.jsx'
+import Select from '../../ui/lib/select.jsx'
 
 const generateFormFieldProps = (
   name, label, hint, isReq,
@@ -25,30 +28,35 @@ const generateFormFieldProps = (
 const loadFormData = () => {
   const savedData = localStorage.getItem('addListingsForm')
   return savedData ? JSON.parse(savedData) : {
-    is_rental: '0', address: '', zip_code: ''
+    is_rental: '0', address: '', zip_code: '', region: ''
   }
 }
 
 const AddListingPage = () => {
+  const regionOptions = useSelector(state => state.regions)
   const dispatch = useDispatch()
+  const [citiOptions, setCitiOptions] = useState([])
 
+  const addAgentModalRef = useRef(null)
   const {
     register,
     watch,
     handleSubmit,
+    control,
     formState: { errors, dirtyFields, touchedFields }
   } = useForm({ mode: 'onBlur', defaultValues: loadFormData() })
 
   const formValues = watch()
   useEffect(() => {
     localStorage.setItem('addListingsForm', JSON.stringify(formValues))
-  },[formValues])
+  }, [formValues])
 
   useEffect(() => {
-    // getCities().then(res => {
+    getCities().then(res => {
+      setCitiOptions(res)
     // console.log(res)
     // dispatch(setCities(res))
-    // })
+    })
   }, [])
 
 
@@ -57,6 +65,9 @@ const AddListingPage = () => {
   }, [formValues])
 
   const validationSchema = {
+    is_rental: {
+      required: 'აირჩიეთ გარიგების ტიპი'
+    },
     address: {
       required: 'ჩაწერეთ ვალიდური მონაცემები',
       minLength: {
@@ -70,6 +81,12 @@ const AddListingPage = () => {
         value: /^[0-9]+$/,
         message: 'მხოლოდ რიცხვები'
       }
+    },
+    region: {
+      required: 'აირჩიეთ რეგიონი'
+    },
+    city: {
+      required: 'აირჩიეთ ქალაქი'
     }
   }
 
@@ -79,64 +96,74 @@ const AddListingPage = () => {
   }
 
   return (
-    <form className={classes['add-listing-form']}
-          onSubmit={handleSubmit(onSubmit)}>
-      <h2 className={classes['main-header']}>ლისტინგის დამატება</h2>
+    <>
+      <form className={classes['add-listing-form']}
+            onSubmit={handleSubmit(onSubmit)}>
+        <h2 className={classes['main-header']}>ლისტინგის დამატება</h2>
 
-      <h4 className={classes['radioGroup-header']}>გარიგების ტიპი</h4>
-      <div className={classes.radioGroup}>
-        <label className={classes.radioLabel}>
-          <input
-            type="radio"
-            name={'is_rental'}
-            value={0}
-            {...register('is_rental', { required: true })}
-            className={classes.radioInput}
+        <h4 className={classes['radioGroup-header']}>გარიგების ტიპი</h4>
+        <div className={classes.radioGroup}>
+          <label className={classes.radioLabel}>
+            <input
+              type="radio"
+              name={'is_rental'}
+              value={0}
+              {...register('is_rental', { required: true })}
+              className={classes.radioInput}
+            />
+            <span className={classes.radioCustom}></span>
+            იყიდება
+          </label>
+
+          <label className={classes.radioLabel}>
+            <input
+              type="radio"
+              name={'is_rental'}
+              value={1}
+              {...register('is_rental', { required: true })}
+              className={classes.radioInput}
+            />
+            <span className={classes.radioCustom}></span>
+            ქირავდება
+          </label>
+        </div>
+
+        <h4 className={classes.heading}>მდებარეობა</h4>
+        <div className={classes['grid-col-2']}>
+          <Input
+            {...generateFormFieldProps('address', 'მისამართი',
+              'მინიმუმ ორი სიმბოლო', true,
+              {
+                register,
+                errors,
+                validationSchema,
+                dirtyFields,
+                touchedFields
+              })}/>
+          <Input
+            {...generateFormFieldProps('zip_code', 'საფოსტო ინდექსი',
+              'მხოლოდ რიცხვები', true,
+              {
+                register,
+                errors,
+                validationSchema,
+                dirtyFields,
+                touchedFields
+              })}/>
+
+          <Select
+            label="რეგიონი"
+            name="region"
+            control={control}
+            options={regionOptions}
           />
-          <span className={classes.radioCustom}></span>
-          იყიდება
-        </label>
 
-        <label className={classes.radioLabel}>
-          <input
-            type="radio"
-            name={'is_rental'}
-            value={1}
-            {...register('is_rental', { required: true })}
-            className={classes.radioInput}
-          />
-          <span className={classes.radioCustom}></span>
-          ქირავდება
-        </label>
-      </div>
-
-      <h4 className={classes.heading}>მდებარეობა</h4>
-      <div className={classes['grid-col-2']}>
-        <Input
-          {...generateFormFieldProps('address', 'მისამართი',
-            'მინიმუმ ორი სიმბოლო', true,
-            {
-              register,
-              errors,
-              validationSchema,
-              dirtyFields,
-              touchedFields
-            })}/>
-        <Input
-          {...generateFormFieldProps('zip_code', 'საფოსტო ინდექსი',
-            'მხოლოდ რიცხვები', true,
-            {
-              register,
-              errors,
-              validationSchema,
-              dirtyFields,
-              touchedFields
-            })}/>
-
-      </div>
+        </div>
 
 
-    </form>
+      </form>
+      <AddAgentModal ref={addAgentModalRef}/>
+    </>
   )
 }
 
--- src/ui/lib/select.jsx
+++ src/ui/lib/select.jsx
@@ -1,104 +1,55 @@
-import { useState } from 'react'
-import Select, { components } from 'react-select'
-import './select.scss'
+import React, { useState } from 'react'
+import { Controller } from 'react-hook-form'
+import styles from './select.module.scss'
 
-export const CustomOption = (props) => {
-  return (
-    <components.Option {...props}>
-      {props.option}
-    </components.Option>
-  )
-}
-
-const CustomMenuList = (props) => {
-  const handleCloseMenu = () => props.setMenuIsOpen(false)
-
-  return (
-    <components.MenuList {...props}>
-      <h4>{props.menuHeader}</h4>
-      {props.customMenuListContent}
-      <div className="button-container">
-        <button className="submit-button" onClick={handleCloseMenu}>
-          Submit
-        </button>
-      </div>
-    </components.MenuList>
-  )
-}
-
-const CustomDropdownIndicator = (props) => {
-  const { menuIsOpen } = props.selectProps
-  return (
-    <components.DropdownIndicator {...props}>
-      {menuIsOpen ? (
-        <span style={{ transform: 'rotate(180deg)' }}>&#x25BC;</span>
-      ) : (
-        <span>&#x25BC;</span>
-      )}
-    </components.DropdownIndicator>
-  )
-}
-
-const CustomSelect = ({
+const Select = ({
+  label,
+  name,
+  control,
   options,
-  isMulti,
-  customMenuListContent,
-  option,
-  placeholder,
-  menuHeader
+  defaultValue = null,
+  onBtnClick = undefined
 }) => {
-  const [menuIsOpen, setMenuIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <Select
-      options={options}
-      isMulti={isMulti}
-      components={{
-        MenuList: (props) => <CustomMenuList {...props}
-                                             setMenuIsOpen={setMenuIsOpen}
-                                             menuHeader={menuHeader}
-                                             customMenuListContent={customMenuListContent &&
-                                               customMenuListContent({
-                                                 ...props,
-                                                 setMenuIsOpen
-                                               })}/>,
-        Option: (props) => option &&
-          (<CustomOption {...props} menuIsOpen={menuIsOpen}
-                         option={option && option(props)}/>),
-        DropdownIndicator: CustomDropdownIndicator
-      }}
-      closeMenuOnSelect={false}
-      hideSelectedOptions={false}
-      menuIsOpen={menuIsOpen}
-      onMenuOpen={() => setMenuIsOpen(true)}
-      onMenuClose={() => setMenuIsOpen(false)}
-      controlShouldRenderValue={false}
-      placeholder={placeholder}
-      isClearable={false}
-      isSearchable={false}
-      inputValue={''}
-      styles={{
-        menu: (provided) => ({
-          ...provided,
-          width: '730px',
-          padding: '24px 24px 28px'
-        }),
-        control: (provided, state) => ({
-          ...provided,
-          cursor: 'pointer',
-          height: '35px',
-          minHeight: 'auto',
-          width: 'fit-content',
-          border: 'none',
-          boxShadow: 'none',
-          backgroundColor: state.isFocused ? '#f0f0f0' : 'white'
-        }),
-        indicatorSeparator: () => ({
-          display: 'none'
-        })
-      }}
+    <Controller
+      name={name}
+      control={control}
+      defaultValue={defaultValue}
+      render={({ field }) => (
+        <div className={styles.dropdownWrapper}>
+          <label className={styles.label}>{label}</label>
+          <div
+            className={styles.dropdown}
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <div className={styles.dropdownSelected}>
+              {field.value ? field.value.name : ''}
+            </div>
+            <div
+              className={`${styles.dropdownMenu} ${isOpen ? styles.show : ''}`}
+            >
+              {onBtnClick && <div className={styles.dropdownItem}
+                                  onClick={onBtnClick}>button</div>}
+              {options.map((option) => (
+                <div
+                  key={option.id}
+                  className={styles.dropdownItem}
+                  onClick={() => {
+                    field.onChange(option)
+                    setIsOpen(false)
+                  }}
+                >
+                  {option.name}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
     />
   )
 }
 
-export default CustomSelect
+export default Select
